5f893b03ef4af3b50d8dc49b24f0c53aee719a08
Sumeet Sonkusare <83730448+sumeet-sonkusare@users.noreply.github.com>
2021-05-25 17:40:38 +0530

    Update Day_02.sql

--- Day_02.sql
+++ Day_02.sql
@@ -20,3 +20,44 @@ WHERE
     CITY REGEXP '^[aeiou]'
     AND
     CITY REGEXP '[aeiou]$';
+
+--2. Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+--https://www.hackerrank.com/challenges/weather-observation-station-9/problem?h_r=next-challenge&h_v=zen
+
+SELECT 
+    DISTINCT(CITY)
+FROM
+    STATION
+WHERE
+    CITY REGEXP '^[^aeiou]';
+
+--3. Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+--https://www.hackerrank.com/challenges/weather-observation-station-10/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT
+    DISTINCT(CITY)
+FROM
+    STATION
+WHERE
+    CITY REGEXP '[^aeiou]$';
+
+--4. Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+--https://www.hackerrank.com/challenges/weather-observation-station-11/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT
+    DISTINCT(CITY)
+FROM 
+    STATION
+WHERE
+    CITY REGEXP '^[^aeiou] | [^aeiou]$';
+    
+--5. Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+--https://www.hackerrank.com/challenges/weather-observation-station-12/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+SELECT
+    DISTINCT(CITY)
+FROM 
+    STATION
+WHERE
+    CITY REGEXP '^[^aeiou]' 
+    AND 
+    CITY REGEXP '[^aeiou]$';
