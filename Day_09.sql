5bdfc8c7fcbb63df9d203e224ae779d12713bb0e
Sumeet Sonkusare <83730448+sumeet-sonkusare@users.noreply.github.com>
2021-06-01 20:17:52 +0530

    Update Day_09.sql

--- Day_09.sql
+++ Day_09.sql
@@ -1,1 +1,1 @@
-##[DATA](https://github.com/sumeet-sonkusare/8-Week-SQL-Challenge/tree/main/Case%20Study%201%20-%20Danny's%20Diner)
+--[DATA](https://github.com/sumeet-sonkusare/8-Week-SQL-Challenge/tree/main/Case%20Study%201%20-%20Danny's%20Diner)
