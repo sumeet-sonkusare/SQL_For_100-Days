dd657aeaaddd9069a46140e6da2e1edc4b175804
Sumeet Sonkusare <83730448+sumeet-sonkusare@users.noreply.github.com>
2021-06-03 02:14:35 +0530

    Create Day_11.sql

---
+++ Day_11.sql
@@ -0,0 +1,14 @@
+--Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+--https://www.hackerrank.com/challenges/asian-population/problem
+
+SELECT 
+    SUM(CITY.POPULATION)
+FROM 
+    CITY
+JOIN 
+    COUNTRY
+ON 
+    CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE 
+    COUNTRY.CONTINENT = 'Asia';
