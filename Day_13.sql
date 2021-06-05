56db1eb4f8853c2f6a7e0b00da36da56ffb413b1
Sumeet Sonkusare <83730448+sumeet-sonkusare@users.noreply.github.com>
2021-06-05 21:34:40 +0530

    Create Day_13.sql

---
+++ Day_13.sql
@@ -0,0 +1,15 @@
+--Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) 
+----and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+----Query the Euclidean Distance between points P1 and P2 and format your answer to display  decimal digits.
+--https://www.hackerrank.com/challenges/weather-observation-station-19/problem
+
+
+SELECT 
+    ROUND(SQRT(POWER(Lat_Dist, 2) + POWER(Long_Dist, 2)),4) AS Distance
+FROM
+    (
+        SELECT
+            MAX(LAT_N) - MIN(LAT_N) AS Lat_Dist, MAX(LONG_W) - MIN(LONG_W) AS Long_Dist
+        FROM
+            STATION
+    ) AS Lat_Long_dist;
