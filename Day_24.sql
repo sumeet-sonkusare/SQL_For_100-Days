5a6763fb1afe004e7ba84c2c790fc0eb166e7e33
Sumeet Sonkusare <83730448+sumeet-sonkusare@users.noreply.github.com>
2021-06-16 07:06:48 +0530

    Update Day_24.sql

--- Day_24.sql
+++ Day_24.sql
@@ -1,5 +1,24 @@
 --Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
 --https://leetcode.com/problems/customers-who-never-order/
+Customers	
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+
+Orders
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+
+
 
 SELECT 
     Name AS Customers
@@ -12,3 +31,10 @@ ON
 WHERE 
     CustomerId IS NULL;
     
+Output
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
