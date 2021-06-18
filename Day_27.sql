67feb58fa589a586b7fdb4f905cd62e757825487
Sumeet Sonkusare <83730448+sumeet-sonkusare@users.noreply.github.com>
2021-06-18 17:58:59 +0530

    Create Day_27.sql

---
+++ Day_27.sql
@@ -0,0 +1,56 @@
+--1. Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring". Return the result table in descending order by rating.
+--https://leetcode.com/problems/not-boring-movies/
+
+--Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+
+SELECT *
+FROM Cinema
+WHERE id%2=1 AND description <> 'boring'
+ORDER BY rating DESC;  
+
+--Output
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+
+
+--2. Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temp table(s).
+--https://leetcode.com/problems/swap-salary/
+
+--Table: Salary
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+
+UPDATE Salary
+SET sex = CASE 
+    WHEN sex='f' THEN 'm'
+    ELSE 'f'
+    END;
+    
+ --Result table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
